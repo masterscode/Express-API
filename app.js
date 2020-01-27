832cbbec35ad673f2e5fdbe6b400e61bdd5ea848
Ogbinaka Emmanuel <ogbinakaemmanuel@gmail.com>
2020-01-27 16:57:40 +0100

    commit from mall, cant tell where the heck i stopped!

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const express = require ('express');
+const express = require("express"); 
+const app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
+//======API CONFIGS HERE======================
+app.use('/api', bookRouter)
+
+
+//=============================================
+
+bookRouter.route('/books')
+.get((req, res)=>{
+  let response  = {hello: 'welcome to my api'}
+  res.json(response);
+});
+
+
+app.get("/", (req, res) => {
+  console.log (req);
+  res.send("welcome   app");
+});
+
+app.listen(port, () => console.log(`running on port ${port}`));
