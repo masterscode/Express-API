4d327d4e76d974cee4126bae6a06ec224342ad73
masterscode <ogbinakaemmanuel@gmail.com>
2021-10-03 23:47:19 +0100

    ...

--- config/db-connect.js
+++ config/db-connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const link = 'mongodb+srv://imanuel:peace2lover@cluster0.mbdwx.mongodb.net/test';
+
 mongoose
-  .connect(link)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("connection successful"))
   .catch((error) => console.log("error with connection", error));
 
